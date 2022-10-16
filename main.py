ccd7f1cff337836e2fbbe465e0ffd5a49cf58c74
tanmay8146 <rv3nom@outlook.com>
2022-10-16 15:14:22 +0530

    v1.1

---
+++ main.py
@@ -0,0 +1,18 @@
+import pricechart as chart
+
+from os import system
+
+system('clear')
+
+        # Author @TanmayXD
+
+ch = int(input("INPUT 1 FOR PRICE CLOSING CHART GENERATOR."))
+
+
+if ch == 1:
+    asset = input("ENTER SYMBOL: ")
+    start = input("ENTER START DATE IN YYYY-MM-DD:")
+    end = input("ENTER END DATE YYYY-MM-DD: ")
+
+    
+
